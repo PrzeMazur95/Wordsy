4ba96c64be5f133e7326c123bb37b402f27e3764
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-02-23 22:47:09 +0100

    Add polishTranslation model

---
+++ app/Models/PolishTranslation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PolishTranslation extends Model
+{
+    use HasFactory;
+
+}
