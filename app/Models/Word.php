b72b6954ef68fa6b7965f4298733022b137eb469
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-02-23 22:57:37 +0100

    Add Polish Translation relationshi to Word model

--- app/Models/Word.php
+++ app/Models/Word.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Word extends Model
 {
     use HasFactory;
 
     protected $fillable = ['word', 'in_polish', 'example', 'difficulty'];
+
+    /**
+     * Return Polish translation
+     *
+     * @return HasOne
+     */
+    public function PolishTranslation(): HasOne
+    {
+        return $this->hasOne(PolishTranslation::class);
+    }
 }
