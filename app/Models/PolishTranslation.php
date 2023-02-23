63ed65d81eced0f6a053b7f0080a15da1603fba5
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-02-23 22:49:21 +0100

    Add relatinship between translation and original word

--- app/Models/PolishTranslation.php
+++ app/Models/PolishTranslation.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PolishTranslation extends Model
 {
     use HasFactory;
 
+    /**
+     * Return relation between Polish translation and original word
+     *
+     * @return BelongsTo
+     */
+    public function word(): BelongsTo
+    {
+        return $this->belongsTo(Word::class);
+    }
 }
