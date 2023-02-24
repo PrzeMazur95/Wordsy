a733d21ccd834fabdae04bdece4a44976562909e
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-02-24 19:46:36 +0100

    Fill index method in WordController

--- app/Http/Controllers/Api/WordController.php
+++ app/Http/Controllers/Api/WordController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Word;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -11,9 +13,9 @@ class WordController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
+    public function index(): JsonResponse
     {
-        //
+        return response()->json(Word::all());
     }
 
     /**
