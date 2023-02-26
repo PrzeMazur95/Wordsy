f0d8931ec5e4f0c2f555ec526d38ed850084f4ba
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-02-26 17:50:10 +0100

    Add enum nmsg when entity has been updated

--- app/Enum/Api/JsonResponseMessages.php
+++ app/Enum/Api/JsonResponseMessages.php
@@ -7,4 +7,6 @@ namespace App\Enum\Api;
 enum JsonResponseMessages: string
 {
     case SAVED = "New entity has been saved";
+    case NOT_FOUND = 'There is no such entity with provided id';
+    case UPDATED = 'This entity has been successfully updated';
 }
