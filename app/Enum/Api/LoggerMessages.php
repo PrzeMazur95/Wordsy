9d38584637b37e5583c6d6b3798c547596ffa06e
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-02-27 21:07:55 +0100

    Add enum msg when smth went wrong with getting specific word through API

--- app/Enum/Api/LoggerMessages.php
+++ app/Enum/Api/LoggerMessages.php
@@ -9,4 +9,5 @@ enum LoggerMessages: string
     case ALL_WORDS = "Something went wrong when trying to get all words from database through API";
     case STORE_WORD = "Something went wrong when trying save a word through API";
     case UPDATE_WORD = "Something went wrong when trying to update provided word through API";
+    case SHOW_WORD = "Something went wrong when trying to get specific word through API";
 }
