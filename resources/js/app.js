b5f8e546364e2599b145cc84c1af8fab1ca4da71
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-03-12 22:01:05 +0100

    Mount vue app into app div

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
 import './bootstrap';
+import { createApp } from "vue";
+import app from "./app.vue";
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+createApp(app).mount("#app");
