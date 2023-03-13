fc76f3b8fffcc65a0ddb7ddc5d570ee5b17014b2
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-03-13 23:05:48 +0100

    Import bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
+import "bootstrap/dist/css/bootstrap.css"
 import './bootstrap';
 import { createApp } from "vue";
 import app from "./app.vue";
 
 createApp(app).mount("#app");
+import "bootstrap/dist/js/bootstrap";
