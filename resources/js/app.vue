9bd41b83037db7f834f72ce58f0c90f1392485f6
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-03-17 21:49:15 +0100

    Mount content page into app

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,12 +1,15 @@
 <template>
     <navbar></navbar>
+    <content></content>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Content from './components/Content.vue'
 export default {
     name: 'App',
     components: {
+        Content,
         Navbar
     }
 }
