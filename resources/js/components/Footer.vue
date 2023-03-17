1fc3739e945d24d89c263f9a7e36db2d2a607a83
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-03-17 22:01:41 +0100

    Add basic footer component

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="footer mt-auto py-3 bg-light">
+        <div class="container text-center">
+            <span>Some text will be placed here</span>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+}
+</script>
