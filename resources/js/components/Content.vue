b864b6930c1c07a58db720659cf43448da4a9f8e
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-03-17 21:48:28 +0100

    Add basic content page for testing purposes

---
+++ resources/js/components/Content.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="row">
+        <div class="card col-3">
+            <img class="card-img-top" src="https://picsum.photos/id/237/800/1200" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-3">
+            <img class="card-img-top" src="https://picsum.photos/id/227/800/1200" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-3">
+            <img class="card-img-top" src="https://picsum.photos/id/217/800/1200" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-3">
+            <img class="card-img-top" src="https://picsum.photos/id/247/800/1200" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Content",
+}
+</script>
