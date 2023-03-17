30b3829f003623ed0389dc1ecfd96658dcc10efd
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-03-17 22:02:03 +0100

    Mount created footer into app container

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,16 +1,19 @@
 <template>
     <navbar></navbar>
     <content></content>
+    <Footer></Footer>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
 export default {
     name: 'App',
     components: {
         Content,
-        Navbar
+        Navbar,
+        Footer
     }
 }
 </script>
