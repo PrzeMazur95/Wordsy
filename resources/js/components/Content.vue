34ac8fec77bbc65b5cf63f7ed4b62fb42b1926af
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-03-19 23:20:06 +0100

    Add example method and data to check if basics works fine

--- resources/js/components/Content.vue
+++ resources/js/components/Content.vue
@@ -8,7 +8,7 @@
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
-        <div class="card col-3">
+        <div class="card col-3" @click="logtext">
             <img class="card-img-top" src="https://picsum.photos/id/227/800/1200" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
@@ -16,10 +16,10 @@
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
-        <div class="card col-3">
+        <div class="card col-3" @click="count++">
             <img class="card-img-top" src="https://picsum.photos/id/217/800/1200" alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">{{ count }}</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
@@ -38,5 +38,15 @@
 <script>
 export default {
     name: "Content",
+    methods: {
+        logtext() {
+            console.log('test text');
+        }
+    },
+    data() {
+        return {
+            count: 0
+        }
+    }
 }
 </script>
