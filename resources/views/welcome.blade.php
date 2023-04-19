9266455abfcb441fec5303ace70a80102c927e86
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-04-19 21:36:28 +0200

    Tittle change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Wordsy</title>
 
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         @vite(['resources/js/app.js', 'resources/css/app.css'])
