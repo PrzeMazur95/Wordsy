fb18fdc2321cf249eb17cba7f9b92e31d8ba63ee
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-04-19 21:28:13 +0200

    Text allign change in content component

--- resources/js/components/Content.vue
+++ resources/js/components/Content.vue
@@ -3,24 +3,24 @@
         <div class="card col-4">
             <img class="card-img-top" src="https://picsum.photos/id/237/800/1200" alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">Add new Word!</h5>
-                <p class="card-text">Do you have some new word which you should remember? Don't wait, save it!</p>
+                <h5 class="card-title text-center">Add new Word!</h5>
+                <p class="card-text text-center">Do you have some new word which you should remember? Don't wait, save it!</p>
             </div>
             <a href="#" class="btn btn-primary">Add new!</a>
         </div>
         <div class="card col-4" @click="logtext">
             <img class="card-img-top" src="https://picsum.photos/id/227/800/1200" alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">All words</h5>
-                <p class="card-text">Do you want to see all stored words? It would be a long list... but that is great! Let's go !</p>
+                <h5 class="card-title text-center">All words</h5>
+                <p class="card-text text-center">Do you want to see all stored words? It would be a long list... but that is great! Let's go !</p>
             </div>
             <a href="#" class="btn btn-primary">Check all!</a>
         </div>
         <div class="card col-4">
             <img class="card-img-top" src="https://picsum.photos/id/217/800/1200" alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">Chellenge!</h5>
-                <p class="card-text">Do you want to chellenge yourself? Go on and check, if you remember all your stored words</p>
+                <h5 class="card-title text-center">Challenge!</h5>
+                <p class="card-text text-center">Do you want to chellenge yourself? Go on and check, if you remember all your stored words</p>
             </div>
             <a href="#" class="btn btn-primary">Try yourself!</a>
         </div>
