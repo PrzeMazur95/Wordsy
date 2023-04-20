c4d4a1e1747996c481fc145faa503230a7f98cef
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-04-20 22:46:14 +0200

    Add addWord js file to be able to have multiple pages with vue components

---
+++ resources/js/addWord.vue
@@ -0,0 +1,16 @@
+<template>
+    <navbar></navbar>
+    <addword></addword>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Addword from './components/addWordForm.vue'
+export default {
+    name: 'addWord',
+    components: {
+        Navbar,
+        Addword
+    }
+}
+</script>
