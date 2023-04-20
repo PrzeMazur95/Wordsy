bb6d9e94aca42de20a20a6df7440d7066cf48093
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-04-20 22:45:24 +0200

    Fill addWord vue with its structure

---
+++ resources/js/addWord.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.css"
+import './bootstrap';
+import { createApp } from "vue";
+import app from "./addWord.vue";
+
+createApp(app).mount("#addWord");
+import "bootstrap/dist/js/bootstrap.js";
