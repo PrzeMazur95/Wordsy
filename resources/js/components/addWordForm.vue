0e9bd31abd766d6009ec4219934cab47ceea5272
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-04-20 22:37:33 +0200

    Add sample form to add new word

--- resources/js/components/addWordForm.vue
+++ resources/js/components/addWordForm.vue
@@ -1,7 +1,39 @@
 <template>
-    <div>
-        Form to add new wod
-    </div>
+    <form class="m-2">
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Example select</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect2">Example multiple select</label>
+            <select multiple class="form-control" id="exampleFormControlSelect2">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <div class="justify-content-center">
+            <button class="btn btn-dark">
+                some text
+            </button>
+        </div>
+    </form>
 </template>
 
 <script>
