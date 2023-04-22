32f5e81ed09afc1fd1d8b2b6bd64360afd4d5eb5
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-04-22 19:03:09 +0200

    Change label to a p tags

--- resources/js/components/addWordForm.vue
+++ resources/js/components/addWordForm.vue
@@ -1,15 +1,15 @@
 <template>
     <form @submit.prevent>
-        <div class="form-group text-center mt-2 mx-4">
-            <label for="word">Enter new word !</label>
+        <div class="form-group text-center mt-2 mx-4 ">
+            <p class ="lead">Enter new word !</p>
             <input class="form-control text-center" v-model="word.name" placeholder="what have you learned today?">
         </div>
         <div class="form-group text-center m-4">
-            <label for="polishTranslation">Provide a translation !</label>
+            <p class ="lead">Provide a translation !</p>
             <input class="form-control text-center" id="polishTranslation" v-model="word.polishTranslation" placeholder="what it means?">
         </div>
         <div class="form-group text-center m-4">
-            <label for="example">Give some example !</label>
+            <p class ="lead">Give some example !</p>
             <textarea class="form-control text-center font-light" id="example" rows="3" v-model="word.example" placeholder="Describe to better remember..."></textarea>
         </div>
         <div class="text-center">
