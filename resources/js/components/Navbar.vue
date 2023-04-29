b63a1aa537f03ddbf99650395d5be763038a3a07
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-04-29 21:46:27 +0200

    Update href in navbar to redircet to homepage

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-dark sticky-top">
-        <a class="navbar-brand text-white m-auto align-content-center" href="#">Wordsy</a>
+        <a class="navbar-brand text-white m-auto align-content-center" href="/">Wordsy</a>
     </nav>
 </template>
 
