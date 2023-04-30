a186c8e8803888a9222a451bf70b85bdb677aa30
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-04-30 16:02:31 +0200

    Import and add small words list to main add word page

--- resources/js/addWord.vue
+++ resources/js/addWord.vue
@@ -1,16 +1,19 @@
 <template>
     <navbar></navbar>
     <addword></addword>
+    <fewnewestwordslist></fewnewestwordslist>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Addword from './components/addWordForm.vue'
+import Fewnewestwordslist from './components/fewNewestWordsList.vue'
 export default {
     name: 'addWord',
     components: {
         Navbar,
-        Addword
+        Addword,
+        Fewnewestwordslist
     }
 }
 </script>
