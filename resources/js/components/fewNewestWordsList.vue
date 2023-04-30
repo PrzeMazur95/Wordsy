61109b114db3a0331ce2a6964f26ddc49cbefbba
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-04-30 16:14:43 +0200

    Add simple array to loop through it and render

--- resources/js/components/fewNewestWordsList.vue
+++ resources/js/components/fewNewestWordsList.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-        List with all words
+    <div class="text-center mt-3">
+        <div v-for="(word, index) in playlist" :key="index">
+            {{index}} - {{word}}
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: "fewNewestWordlist",
+    data() {
+        return {
+            playlist: [
+                'word 1',
+                'word 2'
+            ]
+        }
+    }
 }
 </script>
