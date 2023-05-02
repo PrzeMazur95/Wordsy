b09e91e7df40b9fc722cf0d59f26bdf10e9fb14a
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-05-02 17:52:59 +0200

    Add enum loogger msg when smth went wrong with specific amount of words call

--- app/Enum/Api/LoggerMessages.php
+++ app/Enum/Api/LoggerMessages.php
@@ -11,4 +11,5 @@ enum LoggerMessages: string
     case UPDATE_WORD = "Something went wrong when trying to update provided word through API";
     case SHOW_WORD = "Something went wrong when trying to get specific word through API";
     case DELETE_WORD = "Something went wrong when trying to delete specific word through API";
+    case AMOUNT_OF_WORDS = "Something went wrong when trying to get specific amount of words from database through API";
 }
