4a7b33c3bffa229fd7d7657a7f377ead0f691c23
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-05-05 23:32:10 +0200

    Add all word list blade file

--- resources/views/addWordList.blade.php
+++ resources/views/addWordList.blade.php
@@ -4,13 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Add new Word!</title>
+        <title>List of all Words!</title>
 
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        @vite(['resources/js/addWord.js', 'resources/css/addWord.css'])
+        @vite(['resources/js/allWordList.js', 'resources/css/allWordList.css'])
     </head>
     <body>
-        <div id="addWord">
+        <div id="allWordList">
             <app></app>
         </div>
     </body>
