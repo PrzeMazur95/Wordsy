4fb5c52f80a77fe644696633e6cb60694bbc4c8d
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-05-05 21:32:40 +0200

    Change a little styling of a small list

--- resources/js/components/fewNewestWordsList.vue
+++ resources/js/components/fewNewestWordsList.vue
@@ -2,17 +2,15 @@
     <div class="text-center mt-4">
         <p class="lead">Last added words </p>
     </div>
-    <table class="table text-center mt-2">
+    <table class="table table-sm text-center mt-2">
         <thead>
         <tr>
-            <th scope="col"></th>
             <th scope="col">Word</th>
             <th scope="col">Translation</th>
         </tr>
         </thead>
         <tbody>
         <tr v-for="(word, index) in words" :key="index">
-            <th scope="row">{{word.id}}</th>
             <td>{{word.word}}</td>
             <td>{{word.translation}}</td>
         </tr>
