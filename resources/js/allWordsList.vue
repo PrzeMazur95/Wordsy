92118c32c1e7e71c6942e849c89d08d5ee2984cc
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2023-05-06 17:59:03 +0200

    Add axios request to get list of all words when component is created

--- resources/js/allWordsList.vue
+++ resources/js/allWordsList.vue
@@ -8,6 +8,25 @@ export default {
     name: "allWordsList",
     components: {
         Navbar
+    },
+    data: function () {
+        return {
+            words: []
+        }
+    },
+    methods: {
+        getAllWords() {
+            axios.get('api/Words')
+                .then ( response => {
+                    this.words = response.data.data
+                })
+                .catch( error => {
+                    console.log(error)
+                })
+        }
+    },
+    created() {
+        this.getAllWords();
     }
 }
 </script>
